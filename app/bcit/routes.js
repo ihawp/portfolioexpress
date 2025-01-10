370713b8763ea5594b272a4f386b0ed9439fc41e
ihawp <ihawpebay@gmail.com>
2025-01-09 22:47:51 -0600

    currently leading to error on web-dev route... wasnt before... will look into it tomorrow!

--- app/bcit/routes.js
+++ app/bcit/routes.js
@@ -3,8 +3,8 @@ import time from './middleware.js'
 import { sendHome, sendB } from "./controllers.js";
 import webDev1Router from './web-dev-1/routes.js';
 const bcit = express.Router();
-bcit.use('/web-dev-1', webDev1Router);
 bcit.use(time)
+bcit.use('/web-dev-1', webDev1Router);
 bcit.get('/', sendHome);
 bcit.get('*', sendB);
 export default bcit;
--- server.js
+++ server.js
@@ -5,9 +5,6 @@ import mysql from 'mysql2';
 import compression from 'compression';
 import helmet from 'helmet';
 
-// https://www.freecodecamp.org/news/rest-api-design-best-practices-build-a-rest-api/#heading-architecture
-
-// routes
 import bcit from "./app/bcit/routes.js";
 
 import dbConfig from "./app/db.js";
@@ -15,22 +12,20 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(compression());
+app.use(helmet());
 app.use('/bcit', bcit);
+
+app.get('/', (req, res) => {
+    res.send('aksdjasdsa')
+});
 app.get('*', (req, res) => {
     res.send('This page does not exist.');
 })
 
 /*
-app.use(compression());
-app.use(helmet());
-
-app.use(compression());
 
 const pool = mysql.createPool(dbConfig);
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'gracie')));
-app.use(express.static(path.join(__dirname, 'fathers-day')));
 
 const corsOptions = {
     origin: ['https://ihawp.com', 'https://www.ihawp.com', 'http://localhost:3001', 'http://localhost:3000'],
@@ -52,6 +47,10 @@ app.get('/projects', (req, res) => {
     });
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'gracie')));
+app.use(express.static(path.join(__dirname, 'fathers-day')));
+
 app.get('/gracie', (req, res) => {
     res.sendFile(path.join(__dirname, 'gracie', 'index.html'));
 });
@@ -59,14 +58,6 @@ app.get('/gracie', (req, res) => {
 app.get('/fathers-day', (req, res) => {
     res.sendFile(path.join(__dirname, 'fathers-day', 'index.html'));
 });
-
-// set up router for BCIT
-app.get('/routes', (req, res) => {
-    app.use('/routes/landing-page', express.static('index.html'));
-});
-app.use('/routes/landing-page', express.static('../routes/landing-page'));
-app.use('/routes/web-dev-1/a1', express.static('../routes/wd1-a1'));
-
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
